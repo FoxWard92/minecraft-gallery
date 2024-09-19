c4a511a7be2502375c4577b9ba23ca49a54126f1
FoxMine92 <lampone.frizz@gmail.com>
2024-09-19 21:51:01 +0200

    updates firebase 3.0

--- js/datapacks.js
+++ js/datapacks.js
@@ -15,37 +15,46 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
+let max = 0;
+
 let indexpos = 0;
 
 window.onloads = async function(n) {
     let rb = document.getElementById('rbutton');
     let lb = document.getElementById('lbutton');
-    var max = await countElements();
+
+    var data = await getDataForNode(('data'));
     
     var z = indexpos+n;
 
-    if(z >= 0 && z <= max){   
+    if(z >= 0 && z <= (max/6)){   
         indexpos = z;
+    
     }
 
-    if(((indexpos*6)+6) >= max){
+    if(((indexpos*6)+6) < max){
+        rb.style.opacity = 1;
+    }else{
         rb.style.opacity = 0;
     }
-    if((indexpos*6) <= 0){
+
+    if((indexpos*6) > 0){
+        lb.style.opacity = 1; 
+    }else{
         lb.style.opacity = 0; 
     }
     
-    loadElemnts(indexpos,max);
+    loadElemnts(indexpos,max,data);
     
 };
 
-window.loadElemnts = async function(n,max){
+window.loadElemnts = async function(n,max,data){
     var arr = document.getElementsByClassName('img');
-    var interapt = (n*6); 
-    var data = await getDataForNode(('data'));
+    var interapt = (n*6);
     for(var i = 0; i < 6;i++){
+        arr[i].style.opacity = 0;
         if((interapt+i) < max){
-            var key = '100' + i;
+            var key = '100' + (i+(n*6));
             arr[i].style.opacity = 1;
             arr[i].querySelector('h2').innerText = data[key].title;
             arr[i].querySelector('.l1').innerText = 'Versione : ' + data[key].versione;
@@ -53,34 +62,17 @@ window.loadElemnts = async function(n,max){
             arr[i].querySelector('.l3').innerText = 'Bug Fix  : ' + data[key].bugfix;
             arr[i].style.backgroundImage = `url(${data[key].immagine})`;
             arr[i].querySelector('a').href = data[key].datapack;
-        }else{
-            break;
         }
     }
 };
-
-window.countElements = async function() {
-    try {
-        const snapshot = await get(ref(database, '/data'));
-        if (snapshot.exists()) {
-            return Object.keys(snapshot.val()).length;
-        } else {
-            console.log("Nessun dato trovato");
-            return 0;
-        }
-    } catch (error) {
-        console.error(error);
-        return 0;
-    }
-};
-
 
 async function getDataForNode(nodeId) {
     const dbRef = ref(database, `/${nodeId}`);
     try {
-        const snapshot = await get(dbRef);
+        const snapshot = await get(ref(database, `/${nodeId}`));
         if (snapshot.exists()) {
             const data = snapshot.val();
+            max = Object.keys(snapshot.val()).length;
             return data;
         } else {
             console.log(`No data found for node ${nodeId}`);
