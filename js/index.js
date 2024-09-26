d38f2100ff2afe54297b030aa89567af6860c590
FoxMine92 <lampone.frizz@gmail.com>
2024-09-26 18:19:48 +0200

    UPDATES

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ let z = 0
 
 function clicklink(n){
     const button = document.getElementsByClassName("link");
-    button[z].classList.remove("active-button")
-    button[n].classList.add("active-button")
+    button[z].style.transform = 'translateY(0vh)'
+    button[n].style.transform = 'translateY(0.5vw)'
     z = n
 }
