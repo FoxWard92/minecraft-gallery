f7e472dc34d23b9a940c67b92d08dafb3dc9f61a
FoxMine92 <foxwardmine92@gmail.com>
2024-11-09 18:23:39 +0100

    asd

--- js/home.js
+++ js/home.js
@@ -5,17 +5,19 @@ window.onloads = async function() {
 };
 
 window.loadElemnts = function(max,data){
-    const arr = document.getElementsByClassName('img');
-    for(var i = 3; i < 6;i++){
-        if((i-3) <= max){
-            const key = data['100' + (i-3)];
-            const itemdata = arr[i];
-            itemdata.style.opacity = 1;
-            itemdata.querySelector('h2').innerText = '[NUOVO] ' + key.title;
-            itemdata.style.backgroundImage = `url(${key.immagine})`;
-            itemdata.querySelector('p').innerText = key.descrizione;
-        }else{
-            arr[i].style.opacity = 0;
+    if(data != null){
+        const arr = document.getElementsByClassName('img');
+        for(var i = 3; i < 6;i++){
+            if((i-3) <= max){
+                const key = data['100' + (i-3)];
+                const itemdata = arr[i];
+                itemdata.style.opacity = 1;
+                itemdata.querySelector('h2').innerText = '[NUOVO] ' + key.title;
+                itemdata.style.backgroundImage = `url(${key.immagine})`;
+                itemdata.querySelector('p').innerText = key.descrizione;
+            }else{
+                arr[i].style.opacity = 0;
+            }
         }
     }
 };
